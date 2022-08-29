77f8fa892414efa7b399ace15bacba1047062cbc
dhuvaraggn_AjithRaj <34213472+Dhuvaraggn@users.noreply.github.com>
2022-08-29 19:04:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app =express();
 app.get("/",(req,res)=>{
-    res.send("welcome to ajith jenkins & codebuild image deployed")
+    res.send("welcome to ajith jenkins & codebuild image deployed*************")
 })
 app.listen(8000,"0.0.0.0");
 console.log("application started on port 8000")
